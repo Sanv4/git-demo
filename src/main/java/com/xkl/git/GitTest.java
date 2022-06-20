b93c6725ad39dc49b0c24d508133bc97df3f2d53
Sanv4 <2114228455@qq.com>
2022-06-20 11:54:43 +0800

    first commit

---
+++ src/main/java/com/xkl/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xkl.git;
+
+/**
+ * @author xkl
+ * @version V1.0.0
+ * @since 2022/6/20 11:41
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("java万岁!!!");
+    }
+}
